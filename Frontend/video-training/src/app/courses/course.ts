e1de1f15068fe56f28c90dec92a310973849a5ca
alaaamr19 <54320354+alaaamr19@users.noreply.github.com>
2020-08-12 18:57:59 +0200

    remove old folder

--- Frontend/video-training/src/app/courses/course.ts
+++
@@ -1,8 +0,0 @@
-import { User } from '../users/user';
-export interface Course {
-  _id: number;
-  name: string;
-  description: string;
-  points: number;
-  users: User[];
-}
--- Frontend/video-training/src/app/users/user.ts
+++
@@ -1,8 +0,0 @@
-import { Course } from '../courses/course';
-export interface User {
-  _id: number;
-  name: string;
-  email: string;
-  isAdmin: boolean;
-  courses: Course[];
-}
